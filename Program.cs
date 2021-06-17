9489ad63538546a489a2698f669c506ab2720cef
Hope Ndudim <hope.ndudim@thebulb.africa>
2021-06-17 15:11:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeRecordsApp.Staff;
+using System;
+
+namespace EmployeeRecordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Instantiating Employee Class
+            Employee employee1 = new(001, "John Boyega", DateTime.Now);
+            Employee employee2 = new(002, "David Gattah", DateTime.Now);
+            Employee employee3 = new(003, "Temitope Kolawole", DateTime.Now);
+            Employee employee4 = new(004, "Durowoju Oladipo", DateTime.Now);
+            Employee employee5 = new(005, "Ademosun Cephas", DateTime.Now);
+
+            employee1.JobTitle = "Product Designer";
+            employee1.Department = "Product Development";
+
+            employee2.JobTitle = "Accountant";
+            employee2.Department = "Finance";
+
+            employee3.JobTitle = "Backend Engineer";
+            employee3.Department = "Software Development";
+
+            employee4.JobTitle = "DevOps Engineer";
+            employee4.Department = "Software Development";
+
+            employee5.JobTitle = "Backend Engineer";
+            employee5.Department = "Software Development";
+
+            Employee[] employees = new Employee[]
+            {
+                employee1,employee2,employee3,employee4,employee5
+            };
+
+            foreach (Employee i in employees)
+            {
+
+
+                Console.WriteLine(i.staffID + "\t\t" + i.Name + "\t\t" + i.JobTitle + "\t\t" + i.Department + "\t\t" + i.EmploymentDate + "\n");
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Staff/Employee.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRecordsApp.Staff
+{
+   public class Employee
+    {
+        private readonly int _staffID;
+        private readonly string _fullName;
+
+        public string _jobTitle { get; set; }
+
+        public string _department { get; set; }
+
+        private readonly DateTime _employmentDate;
+
+        public Employee(int staffID, string fullName, DateTime employmentDate)
+        {
+            _staffID = staffID;
+            _fullName = fullName;
+            _employmentDate = employmentDate;
+        }
+
+        public int staffID
+        {
+            get => _staffID;
+        }
+
+
+        public string Name
+        {
+            get => _fullName;
+        }
+
+        public DateTime EmploymentDate
+        {
+            get => _employmentDate;
+        }
+
+
+        public string JobTitle
+        {
+            get
+            {
+                return _jobTitle;
+            }
+
+            set
+            {
+                _jobTitle = value;
+            }
+        }
+
+        public string Department
+        {
+            get
+            {
+                return _department;
+            }
+
+            set
+            {
+                _department = value;
+            }
+        }
+
+
+        public void ClockIn()
+        {
+
+        }
+
+        public void ClockOut()
+        {
+
+        }
+
+        public void Work()
+        {
+
+        }
+    }
+}
